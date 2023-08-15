b202b24c811f898da798f5434264fcadae5dac62
CaioAlmeida09 <Caio09092001@gmail.com>
2023-08-15 20:18:42 -0300

    testando Hooks com o TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,73 @@
 import "./stule.css";
-import { useState } from "react";
+import lixo from "./assets/Lixo.svg.svg";
+import check from "./assets/download.jpg";
+import Edit from "./assets/edit_modify_icon-icons.com_72390.png";
+import { useState, useEffect, useRef } from "react";
 
 function App() {
-  const [lista, setLista] = useState([
-    "Corrida matinal",
-    "Café da manhã",
-    "Estudar",
-  ]);
+  const [lista, setLista] = useState([]);
 
   const [input, setInput] = useState("");
-  const [concluido, setConcluido] = useState([]);
+  const [concluido, setConcluido] = useState<string[]>([]);
+  const [edit, setEdit] = useState({
+    edition: false,
+    ativ: "",
+  });
 
+  const TesteRef = useRef<HTMLInputElement>(null);
   function AddAtividade() {
     if (!input) {
       alert("digite uma atividade");
       return;
     }
+    if (edit.edition == true) {
+      SalveEdit();
+      return;
+    }
+
     const NewLista = [...lista, input];
     setLista(NewLista);
     setInput("");
+    localStorage.setItem("@atividades", JSON.stringify([...lista, input]));
   }
-  function deletar(task: string) {
-    const ListDelete = lista.filter((item) => task !== item);
-    setLista(ListDelete);
+  function SalveEdit() {
+    const findIndex = lista.findIndex((item) => item === edit.ativ);
+    const InputAtt = [...lista];
+    InputAtt[findIndex] = input;
+    setLista(InputAtt);
+    setEdit({
+      edition: false,
+      ativ: "",
+    });
+    localStorage.setItem("@atividades", JSON.stringify(InputAtt));
+    setInput("");
   }
+  useEffect(() => {
+    const TarefaSalva = localStorage.getItem("@atividades");
+    if (TarefaSalva) {
+      setLista(JSON.parse(TarefaSalva));
+    }
+  });
+
   function conclusao(task: string) {
     const listDelete = [...concluido, task];
     setConcluido(listDelete);
   }
-
+  function deletar(task: string) {
+    const ListDelete = lista.filter((item) => task !== item);
+    setLista(ListDelete);
+    localStorage.removeItem("@atividades");
+    localStorage.setItem("@atividades", JSON.stringify(ListDelete));
+  }
+  function EditarAtiv(task: string) {
+    setInput(task);
+    //setLista([...lista, task]);
+    setEdit({
+      edition: true,
+      ativ: task,
+    });
+    TesteRef.current?.focus();
+  }
   return (
     <div className="Principal">
       <h1> Lista de atividades a fazer</h1>
@@ -39,31 +78,43 @@ function App() {
           placeholder="digite a próxima tarefa..."
           onChange={(e) => setInput(e.target.value)}
           value={input}
+          ref={TesteRef}
         ></input>
         <button className="addAtiv" onClick={AddAtividade}>
-          {" "}
-          Adicionar atividade
+          {edit.edition ? "Atualizar atividade" : "Adicionar Atividade"}
         </button>
       </section>
-      {lista.map((item) => (
-        <section className="newAtiv" key={item}>
-          <span className="item"> {item}</span>
-          <button className="button" onClick={() => deletar(item)}>
-            {" "}
-            Excluir Atividade
-          </button>
-          <button className="button" onClick={() => conclusao(item)}>
-            {" "}
-            Atividade realizada{" "}
-          </button>
-        </section>
-      ))}
-      <h1> Lista de atividades concluídas</h1>
-      {concluido.map((item) => (
-        <section className="AtivConcluida">
-          <span> {item} </span>
-        </section>
-      ))}
+      {lista.length <= Number([0]) ? (
+        <h2> Ainda não temos atividades selecionadas </h2>
+      ) : (
+        lista.map((item) => (
+          <section className="newAtiv" key={item}>
+            <span className="item"> {item}</span>
+            <button className="button" onClick={() => conclusao(item)}>
+              {" "}
+              <img src={check} alt="imagemDecheck" />
+            </button>
+            <button className="EditAtiv" onClick={() => EditarAtiv(item)}>
+              <img src={Edit} alt="edition" />
+            </button>
+            <button className="button" onClick={() => deletar(item)}>
+              <img src={lixo} />
+            </button>
+          </section>
+        ))
+      )}
+      {concluido.length === 0 ? (
+        <div></div>
+      ) : (
+        <>
+          <h1> Lista de atividades concluídas</h1>
+          {concluido.map((item) => (
+            <section className="AtivConcluida" key={item}>
+              <span className="itemConcluido"> {item} </span>
+            </section>
+          ))}
+        </>
+      )}
     </div>
   );
 }
